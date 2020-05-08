1928506765bcd1f7b79a63e9301fbc9b05c9fe40
mvdoyle <mdoyle@truecoders.io>
2020-05-08 08:49:16 -0500

    Convo ready

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+
+                Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+            }
+            
+        }
+
+       
+    }
+}
